13958d367b8c61a899f71de67b6cf69b92988afe
Y. Vethan <yadamvethan@gmail.com>
2026-03-29 22:27:39 +0530

    removed slash

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = FastAPI()
 origins = [
     # "http://localhost:5173",
     # "http://127.0.0.1:5173",
-    "https://get-healthy-with-diet-ai.netlify.app/"
+    "https://get-healthy-with-diet-ai.netlify.app"
 ]
 
 app.add_middleware(
